4eaf4309b5c9cc710cd110c8f99020e517140ade
santhosh shankar <47842638+santhosh679577@users.noreply.github.com>
2025-06-13 00:03:15 +0530

    Update CheckNumberIsPositive.java

--- day2/CheckNumberIsPositive.java
+++ day2/CheckNumberIsPositive.java
@@ -3,8 +3,8 @@ package week1.day2;
 public class CheckNumberIsPositive {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		// Declare an integer variable 'version' with value 10
+		
+		
         int version = 10;
       // Conditional Statement to check if number is positive
         if (version > 0) {
